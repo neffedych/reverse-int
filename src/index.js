6ec004950b84dda254723c8f583727f4d872e81e
neffedych <nefedov1011@gmail.com>
2020-10-25 23:11:44 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+ module.exports = function reverse (n) {
+ 	if (n < 0){
+			n = -n;
+			return (n.toString().split('').reverse().join(''));
+
+} else {
+		return (n.toString().split('').reverse().join(''));
 }
+  	}
